8f683b04103da58068558b4570aaa0acd7448715
Anastasia Haller <anadana87@gmail.com>
2023-08-11 12:57:28 +0200

    improved sidebar menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
+      menuItems = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,6 +9,12 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
